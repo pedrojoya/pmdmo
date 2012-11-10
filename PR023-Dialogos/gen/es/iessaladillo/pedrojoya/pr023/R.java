770b9133fa2f5808c38adc2b99006661de239001
Pedro Joya <pedrojoya@gmail.com>
2012-11-10 20:55:04 +0100

    PR023 terminado

--- PR023-Dialogos/gen/es/iessaladillo/pedrojoya/pr023/R.java
+++ PR023-Dialogos/gen/es/iessaladillo/pedrojoya/pr023/R.java
@@ -8,6 +8,9 @@
 package es.iessaladillo.pedrojoya.pr023;
 
 public final class R {
+    public static final class array {
+        public static final int turnos=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -21,16 +24,22 @@ public final class R {
         public static final int toast_frame=0x7f020002;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f070000;
-        public static final int btnAlertaSiNo=0x7f070003;
-        public static final int btnDatePicker=0x7f070001;
-        public static final int btnTimePicker=0x7f070002;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int btnAlertaSeleccionDirecta=0x7f080004;
+        public static final int btnAlertaSeleccionMultiple=0x7f080006;
+        public static final int btnAlertaSeleccionSimple=0x7f080005;
+        public static final int btnAlertaSiNo=0x7f080003;
+        public static final int btnDatePicker=0x7f080001;
+        public static final int btnTimePicker=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
         public static final int alerta_de_pregunta_si_no=0x7f050007;
+        public static final int alerta_de_seleccion_directa=0x7f050010;
+        public static final int alerta_de_seleccion_multiple=0x7f050011;
+        public static final int alerta_de_seleccion_simple=0x7f05000d;
         public static final int app_name=0x7f050000;
         public static final int datepicker=0x7f050004;
         public static final int eliminar_usuario=0x7f050008;
@@ -39,12 +48,15 @@ public final class R {
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
         public static final int no=0x7f05000c;
+        public static final int no_ha_seleccionado_ningun_turno=0x7f050012;
+        public static final int selecciona_el_turno_de_trabajo=0x7f05000f;
         public static final int si=0x7f05000a;
         public static final int timepicker=0x7f050006;
         public static final int title_activity_main=0x7f050003;
+        public static final int turno=0x7f05000e;
         public static final int usuario_borrado=0x7f05000b;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
     }
 }
--- PR023-Dialogos/src/es/iessaladillo/pedrojoya/pr023/MainActivity.java
+++ PR023-Dialogos/src/es/iessaladillo/pedrojoya/pr023/MainActivity.java
@@ -1,7 +1,6 @@
 package es.iessaladillo.pedrojoya.pr023;
 
 import java.util.Calendar;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
@@ -12,6 +11,7 @@ import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnMultiChoiceClickListener;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -27,9 +27,14 @@ import android.widget.Toast;
 public class MainActivity extends Activity implements OnDateSetListener, OnTimeSetListener {
 
 	// Constantes
-    private static final int DLG_DATEPICKER = 0;	// Diálogo DatePicker.
-	private static final int DLG_TIMEPICKER = 1;	// Diálogo TimePicker.
-	private static final int DLG_ALERTA_SINO = 2;	// Alerta Si / NO.
+    private static final int DLG_DATEPICKER = 0;
+	private static final int DLG_TIMEPICKER = 1;
+	private static final int DLG_ALERTA_SINO = 2;
+	private static final int DLG_ALERTA_SELECCION_DIRECTA = 3; 
+	private static final int DLG_ALERTA_SELECCION_SIMPLE = 4;
+	private static final int DLG_ALERTA_SELECCION_MULTIPLE = 5;
+	private int turnoSeleccionado;	// Selección simple.
+	private boolean[] turnosSeleccionados;	// Selección múltiple.
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,11 +61,30 @@ public class MainActivity extends Activity implements OnDateSetListener, OnTimeS
 		this.showDialog(DLG_ALERTA_SINO);
 	}
 
+	// Al hacer click sobre btnAlertaSeleccionDirecta.
+	public void btnAlertaSeleccionDirectaOnClick(View v) {
+		// Muestro el diálogo correspondiente.
+		this.showDialog(DLG_ALERTA_SELECCION_DIRECTA);
+	}
+	
+	// Al hacer click sobre btnAlertaSeleccionSimple.
+	public void btnAlertaSeleccionSimpleOnClick(View v) {
+		// Muestro el diálogo correspondiente.
+		this.showDialog(DLG_ALERTA_SELECCION_SIMPLE);
+	}
+
+	// Al hacer click sobre btnAlertaSeleccionMultiple.
+	public void btnAlertaSeleccionMultipleOnClick(View v) {
+		// Muestro el diálogo correspondiente.
+		this.showDialog(DLG_ALERTA_SELECCION_MULTIPLE);
+	}
+	
 	@Override
 	protected Dialog onCreateDialog(int idDialogo) {
 		// Creo el diálogo correspondiente.
 		Dialog dialogo = null;
 		Calendar calendario;
+		AlertDialog.Builder b;
 		switch (idDialogo) {
 		case DLG_DATEPICKER:
 			calendario = Calendar.getInstance(); 
@@ -77,7 +101,7 @@ public class MainActivity extends Activity implements OnDateSetListener, OnTimeS
 							true);
 			break;
 		case DLG_ALERTA_SINO:
-			AlertDialog.Builder b = new AlertDialog.Builder(this);
+			b = new AlertDialog.Builder(this);
 			b.setTitle(R.string.eliminar_usuario);
 			b.setMessage(R.string.esta_seguro_de_que_quiere_eliminar_el_usuario);
 			b.setIcon(R.drawable.ic_launcher);
@@ -88,6 +112,79 @@ public class MainActivity extends Activity implements OnDateSetListener, OnTimeS
 				}
 			});
 			b.setNegativeButton(R.string.no, null);
+			dialogo = b.create();
+			break;
+		case DLG_ALERTA_SELECCION_DIRECTA:
+			b = new AlertDialog.Builder(this);
+			b.setTitle(R.string.turno);
+			b.setItems(R.array.turnos, new OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					String[] turnos = getResources().getStringArray(R.array.turnos);
+					mostrarTostada(getString(R.string.ha_seleccionado) + turnos[which], 
+							R.drawable.ic_launcher);
+				}
+			});
+			b.setIcon(R.drawable.ic_launcher);
+			dialogo = b.create();
+			break;
+		case DLG_ALERTA_SELECCION_SIMPLE:
+			b = new AlertDialog.Builder(this);
+			b.setTitle(R.string.turno);
+			b.setSingleChoiceItems(R.array.turnos, 0, new OnClickListener(){
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					turnoSeleccionado = which;
+				}
+			});
+			b.setIcon(R.drawable.ic_launcher);
+			b.setNeutralButton("Aceptar", new OnClickListener(){
+				@Override
+				public void onClick(DialogInterface d, int arg1) {
+					d.dismiss();
+					String[] turnos = getResources().getStringArray(R.array.turnos);
+					mostrarTostada(getString(R.string.ha_seleccionado) + turnos[turnoSeleccionado], 
+							R.drawable.ic_launcher);
+				}
+			});
+			dialogo = b.create();
+			break;
+		case DLG_ALERTA_SELECCION_MULTIPLE:
+			turnosSeleccionados = new boolean[] {true, false, false};
+			b = new AlertDialog.Builder(this);
+			b.setTitle(R.string.turno);
+			b.setMultiChoiceItems(R.array.turnos, turnosSeleccionados , new OnMultiChoiceClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+					turnosSeleccionados[which] = isChecked;
+				}
+			});
+			b.setIcon(R.drawable.ic_launcher);
+			b.setNeutralButton("Aceptar", new OnClickListener(){
+				@Override
+				public void onClick(DialogInterface d, int arg1) {
+					d.dismiss();
+					String[] turnos = getResources().getStringArray(R.array.turnos);
+					String mensaje = "";
+					boolean primero = true;
+					for (int i = 0; i < turnosSeleccionados.length; i++) {
+						if (turnosSeleccionados[i]) {
+							if (primero) {
+								mensaje += getString(R.string.ha_seleccionado);
+								primero = false;
+							}
+							else {
+								mensaje += ", ";
+							}
+							mensaje += turnos[i];
+						}
+					}
+					if (mensaje.equals("")) {
+						mensaje = getString(R.string.no_ha_seleccionado_ningun_turno);
+					}
+					mostrarTostada(mensaje, R.drawable.ic_launcher);
+				}
+			});
 			dialogo = b.create();
 			break;
 		}
@@ -146,6 +243,5 @@ public class MainActivity extends Activity implements OnDateSetListener, OnTimeS
         // Muestro la tostada.
         tostada.show();
 	}
-		
-    
+
 }
