ba94f12987e3c4da73464dcf2775dbf1e7cce547
Pedro Joya <pedrojoya@gmail.com>
2012-11-19 13:04:10 +0100

    PR025 Finalizado

--- PR025-ListViewSeleccionCheckeable/gen/es/iessaladillo/pedrojoya/pr025/BuildConfig.java
+++ PR025-ListViewSeleccionCheckeable/gen/es/iessaladillo/pedrojoya/pr025/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.pr025_listviewseleccioncheckeable;
+package es.iessaladillo.pedrojoya.pr025;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
