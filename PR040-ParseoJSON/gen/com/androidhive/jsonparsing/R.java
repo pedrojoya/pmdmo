58bb0b62a7081f3e39f2973cd49627478fffaaf1
Pedro Joya <pedrojoya@gmail.com>
2013-02-15 14:33:39 +0100

    PR040 terminado

--- PR040-ParseoJSON/gen/com/androidhive/jsonparsing/R.java
+++ PR040-ParseoJSON/gen/com/androidhive/jsonparsing/R.java
@@ -12,22 +12,24 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int vacia=0x7f020001;
     }
     public static final class id {
-        public static final int email=0x7f050001;
-        public static final int email_label=0x7f050004;
-        public static final int mobile=0x7f050002;
-        public static final int mobile_label=0x7f050005;
-        public static final int name=0x7f050000;
-        public static final int name_label=0x7f050003;
+        public static final int lblEmail=0x7f050001;
+        public static final int lblNombre=0x7f050000;
+        public static final int lblTlfMovil=0x7f050002;
+        public static final int lblVacia=0x7f050005;
+        public static final int lstLista=0x7f050003;
+        public static final int rlVacia=0x7f050004;
     }
     public static final class layout {
-        public static final int list_item=0x7f030000;
+        public static final int fila=0x7f030000;
         public static final int main=0x7f030001;
-        public static final int single_list_item=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int movil=0x7f040002;
+        public static final int no_hay_elementos_que_mostrar=0x7f040003;
     }
 }
