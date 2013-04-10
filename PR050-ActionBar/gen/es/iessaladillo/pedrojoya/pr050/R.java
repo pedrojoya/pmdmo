ba02e781bb4aff5c4ada11a3c25f5975fd0da5e2
Pedro Joya <pedrojoya@gmail.com>
2013-04-10 23:28:54 +0200

    PR050-Cambios menores

--- PR050-ActionBar/gen/es/iessaladillo/pedrojoya/pr050/R.java
+++ PR050-ActionBar/gen/es/iessaladillo/pedrojoya/pr050/R.java
@@ -45,7 +45,7 @@ public final class R {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
         public static final int actionbar=0x7f050001;
