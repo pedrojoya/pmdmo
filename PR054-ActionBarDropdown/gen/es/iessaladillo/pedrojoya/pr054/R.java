f19dd8795007f0131e92c2d207bf0ee445f9e2c5
Pedro Joya Máñez <pedrojoya@gmail.com>
2013-04-16 10:34:21 +0200

    PR055 - Terminado

--- PR054-ActionBarDropdown/gen/es/iessaladillo/pedrojoya/pr054/R.java
+++ PR054-ActionBarDropdown/gen/es/iessaladillo/pedrojoya/pr054/R.java
@@ -83,9 +83,12 @@ public final class R {
     
  API 11 theme customizations can go here. 
 
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
+         Base application theme for API 14+. This theme completely replaces 
+		AppBaseTheme from BOTH res/values/styles.xml and res/values-v11/styles.xml 
+		on API 14+ devices.
+
+
+
     
  API 14 theme customizations can go here. 
          */
---
+++ PR055-ActionBarSherlockDropdown/src/es/iessaladillo/pedrojoya/pr055/fragmentos/NotasFragment.java
@@ -0,0 +1,19 @@
+package es.iessaladillo.pedrojoya.pr055.fragmentos;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import es.iessaladillo.pedrojoya.pr055.R;
+
+public class NotasFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        // Se infla y retorna el layout correspondiente.
+        return inflater.inflate(R.layout.fragment_notas, container, false);
+    }
+
+}
