300069cbd048653145d91d0673fb3f0c94224626
Pedro Joya Máñez <pedrojoya@gmail.com>
2013-04-24 14:07:53 +0200

    PR062 - TERMINADO

--- PR061-TabsFragmentsViewPager/gen/es/iessaladillo/pedrojoya/pr061/R.java
+++ PR061-TabsFragmentsViewPager/gen/es/iessaladillo/pedrojoya/pr061/R.java
@@ -18,6 +18,7 @@ public final class R {
     
          */
         public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_text_size=0x7f040002;
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
@@ -25,22 +26,21 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080002;
-        public static final int pager=0x7f080000;
-        public static final int section_label=0x7f080001;
+        public static final int lblPagina=0x7f080001;
+        public static final int vpPaginador=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main_dummy=0x7f030001;
+        public static final int fragment_pagina=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int title_section1=0x7f050002;
-        public static final int title_section2=0x7f050003;
-        public static final int title_section3=0x7f050004;
+        public static final int pestana=0x7f050003;
+        public static final int titulo=0x7f050001;
     }
     public static final class style {
         /** 
---
+++ PR062-SherlockTabsFragmentsViewPager/src/es/iessaladillo/pedrojoya/pr062/actividades/MainActivity.java
@@ -0,0 +1,142 @@
+package es.iessaladillo.pedrojoya.pr062.actividades;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+
+import es.iessaladillo.pedrojoya.pr062.R;
+import es.iessaladillo.pedrojoya.pr062.fragmentos.PaginaFragment;
+
+public class MainActivity extends SherlockFragmentActivity implements
+        ActionBar.TabListener, OnPageChangeListener {
+
+    ActionBar actionBar;
+    PaginasAdapter adaptador;
+    ViewPager paginador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Se configura la action bar para que muestre pestañas.
+        actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        // Se crea el adaptador que mostrará el fragmento correspondiente y se
+        // le asigna al paginador.
+        adaptador = new PaginasAdapter(getSupportFragmentManager());
+        paginador = (ViewPager) findViewById(R.id.vpPaginador);
+        paginador.setAdapter(adaptador);
+        // Se establece que la actividad actúa como objeto //listener// cuando
+        // se cambie la página en el paginador.
+        paginador.setOnPageChangeListener(this);
+        // Se crea una pestaña para cada página, cuyo título corresponde al de
+        // la página y se establece la propia actividad como objeto listener que
+        // será notificado cuando se seleccione una pestaña.
+        for (int i = 0; i < adaptador.getCount(); i++) {
+            ActionBar.Tab tab = actionBar.newTab();
+            tab.setText(adaptador.getPageTitle(i));
+            tab.setTabListener(this);
+            actionBar.addTab(tab);
+        }
+        // Si se viene de un estado anterior se coloca en el número de pestaña
+        // salvado.
+        if (savedInstanceState != null) {
+            actionBar.setSelectedNavigationItem(savedInstanceState.getInt(
+                    "tab", 0));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Se almacena el número de pestaña actual.
+        outState.putInt("tab", actionBar.getSelectedNavigationIndex());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getSupportMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    // Al seleccionar una pestaña
+    @Override
+    public void onTabSelected(ActionBar.Tab tab,
+            FragmentTransaction fragmentTransaction) {
+        // Se coloca al paginador en dicha página.
+        paginador.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab,
+            FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab,
+            FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int arg0) {
+    }
+
+    @Override
+    public void onPageScrolled(int arg0, float arg1, int arg2) {
+    }
+
+    // Al cambiar la página en el paginador. Recibe el número de página.
+    @Override
+    public void onPageSelected(int position) {
+        // Se establece como pestaña activa la correspondiente a dicho número de
+        // página.
+        actionBar.setSelectedNavigationItem(position);
+    }
+
+    // Adaptador para el paginador de fragmentos.
+    public class PaginasAdapter extends FragmentPagerAdapter {
+
+        // Constantes.
+        private int NUM_PAGINAS = 5;
+
+        // Constructor. Recibe el gestor de fragmentos.
+        public PaginasAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        // Retorna el fragmento correspondiente a una página. Recibe el número
+        // de página.
+        @Override
+        public Fragment getItem(int position) {
+            // Se crea el fragmento y se le pasa como argumento el número de
+            // pestaña para que lo escriba en su TextView.
+            Fragment frgPagina = new PaginaFragment();
+            Bundle parametros = new Bundle();
+            parametros.putInt(PaginaFragment.PAR_NUM_PAGINA, position + 1);
+            frgPagina.setArguments(parametros);
+            return frgPagina;
+        }
+
+        // Retorna el número de páginas.
+        @Override
+        public int getCount() {
+            return NUM_PAGINAS;
+        }
+
+        // Retorna el título de una página. Recibe el número de página.
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getString(R.string.pestana) + (position + 1);
+        }
+    }
+
+}
