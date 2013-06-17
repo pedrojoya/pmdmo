5d22ca974da187f293131dc65cecfcfa32653383
Pedro Joya <pedrojoya@gmail.com>
2013-06-17 22:51:48 +0200

    pr027 nuevo

---
+++ PR027-SQLiteDAO/src/es/iessaladillo/pedrojoya/pr027/fragmentos/ListaAlumnosFragment.java
@@ -0,0 +1,18 @@
+package es.iessaladillo.pedrojoya.pr027.fragmentos;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListaAlumnosFragment extends Fragment {
+
+    // Retorna la vista que debe retornar el fragmento.
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        ;
+    }
+
+}
