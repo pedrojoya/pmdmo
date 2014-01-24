a7e9e60fd54b3e47dc67c07289382a42dee69356
Pedro Joya <pedrojoya@gmail.com>
2014-01-24 19:08:22 +0100

    pr039

--- PR039-SoundPool/src/es/iessaladillo/pedrojoya/pr039/MainActivity.java
+++ PR039-SoundPool/src/es/iessaladillo/pedrojoya/pr039/MainActivity.java
@@ -6,82 +6,86 @@ import android.media.SoundPool;
 import android.media.SoundPool.OnLoadCompleteListener;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
-	private static final float VELOCIDAD_NORMAL = 1f;
-	private static final int PRIORIDAD_MAXIMA = 1;
-	private static final float VOLUMEN_MAX = 1f;
-	private static final int SIN_BUCLE = 0;
-	// Constantes.
-	private static final int CALIDAD_NORMAL = 0;
-	private static final int PRIORIDAD_NORMAL = 1;
+    // Constantes.
+    private static final float VELOCIDAD_NORMAL = 1f;
+    private static final int PRIORIDAD_MAXIMA = 1;
+    private static final float VOLUMEN_MAX = 1f;
+    private static final int SIN_BUCLE = 0;
+    private static final int CALIDAD_NORMAL = 0;
+    private static final int PRIORIDAD_NORMAL = 1;
 
-	// Variables miembro.
-	private SoundPool reproductor;
-	private int idDisparo;
-	private int idExplosion;
-	private Button btnDisparar;
-	private Button btnExplosion;
+    // Variables.
+    private SoundPool reproductor;
+    private int idDisparo;
+    private int idExplosion;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		// Obtengo las vistas.
-		btnDisparar = (Button) this.findViewById(R.id.btnDisparar);
-		btnExplosion = (Button) this.findViewById(R.id.btnExplosion);
-		// Inicialmente los botones estarán deshabilitados hasta que los sonidos
-		// correspondientes estén cargados en el reproductor.
-		btnDisparar.setEnabled(false);
-		btnExplosion.setEnabled(false);
-		// Creo el objeto SoundPool con un límite de 8 sonidos simultáneos y
-		// calidad estándar.
-		reproductor = new SoundPool(8, AudioManager.STREAM_MUSIC,
-				CALIDAD_NORMAL);
-		// Cargo los ficheros de sonido (recibe el contexto, el recurso y la
-		// prioridad estándar).
-		idDisparo = reproductor.load(this, R.raw.disparo, PRIORIDAD_NORMAL);
-		idExplosion = reproductor.load(this, R.raw.explosion, PRIORIDAD_NORMAL);
-		// Cuando se termine de cargar el sonido, se activa el botón
-		// correspondiente.
-		reproductor.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+    // Vistas.
+    private ImageView btnDisparar;
+    private ImageView btnExplosion;
 
-			@Override
-			public void onLoadComplete(SoundPool soundPool, int sampleId,
-					int status) {
-				// Activo el botón correspondiente al sonido.
-				if (sampleId == idDisparo) {
-					btnDisparar.setEnabled(true);
-				} else if (sampleId == idExplosion) {
-					btnExplosion.setEnabled(true);
-				}
-			}
-		});
-	}
+    // Al crear la actividad.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnDisparar = (ImageView) this.findViewById(R.id.btnDisparar);
+        btnExplosion = (ImageView) this.findViewById(R.id.btnExplosion);
+        // Inicialmente los botones estarán deshabilitados hasta que los sonidos
+        // correspondientes estén cargados en el reproductor.
+        btnDisparar.setEnabled(false);
+        btnExplosion.setEnabled(false);
+        // Se crea el objeto SoundPool con un límite de 8 sonidos simultáneos y
+        // calidad estándar.
+        reproductor = new SoundPool(8, AudioManager.STREAM_MUSIC,
+                CALIDAD_NORMAL);
+        // Se cargan los ficheros de sonido (recibe el contexto, el recurso y la
+        // prioridad estándar).
+        idDisparo = reproductor.load(this, R.raw.disparo, PRIORIDAD_NORMAL);
+        idExplosion = reproductor.load(this, R.raw.explosion, PRIORIDAD_NORMAL);
+        // Cuando se termine de cargar el sonido, se activa el botón
+        // correspondiente.
+        reproductor.setOnLoadCompleteListener(new OnLoadCompleteListener() {
 
-	// Al hacer click sobre btnDisparar.
-	public void btnDispararOnClick(View v) {
-		// Reproduzco el disparo.
-		reproductor.play(idDisparo, VOLUMEN_MAX, VOLUMEN_MAX, PRIORIDAD_MAXIMA, SIN_BUCLE, VELOCIDAD_NORMAL);
-	}
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId,
+                    int status) {
+                // Activo el botón correspondiente al sonido.
+                if (sampleId == idDisparo) {
+                    btnDisparar.setEnabled(true);
+                } else if (sampleId == idExplosion) {
+                    btnExplosion.setEnabled(true);
+                }
+            }
+        });
+    }
 
-	// Al hacer click sobre btnExplosion.
-	public void btnExplosionOnClick(View v) {
-		// Reproduzco la explosión.
-		reproductor.play(idExplosion, VOLUMEN_MAX, VOLUMEN_MAX, PRIORIDAD_MAXIMA, SIN_BUCLE, VELOCIDAD_NORMAL);
-	}
+    // Al hacer click sobre btnDisparar.
+    public void btnDispararOnClick(View v) {
+        // Se reproduce el disparo.
+        reproductor.play(idDisparo, VOLUMEN_MAX, VOLUMEN_MAX, PRIORIDAD_MAXIMA,
+                SIN_BUCLE, VELOCIDAD_NORMAL);
+    }
 
-	@Override
-	protected void onDestroy() {
-		// Libero los recursos del reproductor.
-		if (reproductor != null) {
-			reproductor.release();
-			reproductor = null;
-		}
-		super.onDestroy();
-	}
-	
-	
+    // Al hacer click sobre btnExplosion.
+    public void btnExplosionOnClick(View v) {
+        // Se reproduce la explosión.
+        reproductor.play(idExplosion, VOLUMEN_MAX, VOLUMEN_MAX,
+                PRIORIDAD_MAXIMA, SIN_BUCLE, VELOCIDAD_NORMAL);
+    }
+
+    // Al destruir la actividad.
+    @Override
+    protected void onDestroy() {
+        // Se liberan los recursos del reproductor.
+        if (reproductor != null) {
+            reproductor.release();
+            reproductor = null;
+        }
+        super.onDestroy();
+    }
+
 }
