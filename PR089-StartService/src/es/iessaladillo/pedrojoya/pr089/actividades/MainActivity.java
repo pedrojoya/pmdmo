b5089a8b70b1d0d648ad988779d886ce1d3d6117
pedrojoya <pedrojoya@gmail.com>
2014-03-21 23:04:34 +0100

    PR089

---
+++ PR089-StartService/src/es/iessaladillo/pedrojoya/pr089/actividades/MainActivity.java
@@ -0,0 +1,86 @@
+package es.iessaladillo.pedrojoya.pr089.actividades;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import es.iessaladillo.pedrojoya.pr089.R;
+import es.iessaladillo.pedrojoya.pr089.modelos.Cancion;
+import es.iessaladillo.pedrojoya.pr089.servicios.MusicaOnlineService;
+
+public class MainActivity extends Activity implements OnItemClickListener {
+
+    private Intent intentServicio;
+    private ListView lstCanciones;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getVistas();
+        intentServicio = new Intent(getApplicationContext(),
+                MusicaOnlineService.class);
+    }
+
+    private void getVistas() {
+        lstCanciones = (ListView) findViewById(R.id.lstCanciones);
+        ArrayList<Cancion> canciones = new ArrayList<Cancion>();
+        canciones
+                .add(new Cancion("Morning Mood (by Grieg)", "3:43",
+                        "https://www.youtube.com/audiolibrary_download?vid=036500ffbf472dcc"));
+        canciones
+                .add(new Cancion("Brahms Lullaby", "1:46",
+                        "https://www.youtube.com/audiolibrary_download?vid=9894a50b486c6136"));
+        canciones
+                .add(new Cancion("From Russia With Love", "2:26",
+                        "https://www.youtube.com/audiolibrary_download?vid=4e8d1a0fdb3bbe12"));
+        canciones
+                .add(new Cancion("Les Toreadors from Carmen (by Bizet)",
+                        "2:21",
+                        "https://www.youtube.com/audiolibrary_download?vid=fafb35a907cd6e73"));
+        canciones
+                .add(new Cancion("Funeral March (by Chopin)", "9:25",
+                        "https://www.youtube.com/audiolibrary_download?vid=4a7d058f20d31cc4"));
+        ArrayAdapter<Cancion> adaptador = new ArrayAdapter<Cancion>(this,
+                android.R.layout.simple_list_item_1, canciones);
+        lstCanciones.setAdapter(adaptador);
+        lstCanciones.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        lstCanciones.setOnItemClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.mnuParar:
+            stopService(intentServicio);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Cuando se hace click en una canción.
+    @Override
+    public void onItemClick(AdapterView<?> lst, View v, int position, long id) {
+        // Se inicia el servicio para que reproduzca la canción.
+        Cancion cancion = (Cancion) lstCanciones.getItemAtPosition(position);
+        intentServicio.putExtra(MusicaOnlineService.EXTRA_URL_CANCION,
+                cancion.getUrl());
+        startService(intentServicio);
+    }
+
+}
