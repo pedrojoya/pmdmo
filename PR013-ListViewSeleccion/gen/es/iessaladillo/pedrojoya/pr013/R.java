821c490314c7d4d338e54119be4e56a5ff232d50
Pedro Joya <pedrojoya@gmail.com>
2014-06-12 13:55:01 +0200

    PR013_con_puntuacion

--- PR013-ListViewSeleccion/gen/es/iessaladillo/pedrojoya/pr013/R.java
+++ PR013-ListViewSeleccion/gen/es/iessaladillo/pedrojoya/pr013/R.java
@@ -48,19 +48,22 @@ public final class R {
         public static final int pregunta=0x7f020009;
         public static final int pregunta_fondo=0x7f02000a;
         public static final int progreso_fondo=0x7f02000b;
-        public static final int respuesta_fondo=0x7f02000c;
-        public static final int star=0x7f02000d;
+        public static final int puntuacion_fondo=0x7f02000c;
+        public static final int puntuacion_fondo_incorrecto=0x7f02000d;
+        public static final int respuesta_fondo=0x7f02000e;
+        public static final int star=0x7f02000f;
     }
     public static final class id {
-        public static final int btnComprobar=0x7f090005;
-        public static final int contador_animacion=0x7f090007;
+        public static final int btnComprobar=0x7f090006;
+        public static final int contador_animacion=0x7f090008;
         public static final int frlContador=0x7f090002;
-        public static final int lblContador=0x7f090004;
+        public static final int lblContador=0x7f090005;
         public static final int lblPregunta=0x7f090000;
-        public static final int lblRespuesta=0x7f090006;
+        public static final int lblPuntuacion=0x7f090003;
+        public static final int lblRespuesta=0x7f090007;
         public static final int lstRespuestas=0x7f090001;
-        public static final int menu_settings=0x7f090008;
-        public static final int vContador=0x7f090003;
+        public static final int menu_settings=0x7f090009;
+        public static final int vContador=0x7f090004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- PR013-ListViewSeleccion/src/es/iessaladillo/pedrojoya/pr013/MainActivity.java
+++ PR013-ListViewSeleccion/src/es/iessaladillo/pedrojoya/pr013/MainActivity.java
@@ -2,6 +2,8 @@ package es.iessaladillo.pedrojoya.pr013;
 
 import java.util.ArrayList;
 
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.os.Bundle;
@@ -9,6 +11,7 @@ import android.os.CountDownTimer;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.BounceInterpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -16,88 +19,163 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener,
         OnItemClickListener {
 
-    // Variables a nivel de clase.
+    // Vistas.
     private ListView lstRespuestas;
     private TextView lblContador;
-    private ObjectAnimator animator;
     private Button btnComprobar;
     private View vContador;
-    private ArrayAdapter<String> adaptador;
 
+    // Variables.
+    private ObjectAnimator mAnimador;
+    private ArrayAdapter<String> mAdaptador;
+    private int mPuntuacion = 100;
+
+    // Constantes.
+    private long CONTADOR_INICIAL = 5000;
+    private long CONTADOR_INTERVALO = 1000;
+    private TextView lblPuntuacion;
+
+    // Al crear la actividad.
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        // Llamo al onCreate del padre.
         super.onCreate(savedInstanceState);
-        // Establezco el layout que mostrará la actividad.
         setContentView(R.layout.activity_main);
-        // Obtengo las vistas.
+        // Se obtienen e inicializan las vistas.
         getVistas();
     }
 
+    // Al mostrar la actividad.
     @Override
     protected void onResume() {
-        vContador = (View) findViewById(R.id.vContador);
-        animator = ObjectAnimator.ofFloat(vContador, View.ROTATION, 0.0f,
-                10 * 360.0f);
-        animator.setDuration(30000);
-        animator.setRepeatMode(ObjectAnimator.RESTART);
-        animator.setRepeatCount(ObjectAnimator.INFINITE);
-        animator.setInterpolator(new LinearInterpolator());
-        animator.start();
-        CountDownTimer contador = new CountDownTimer(10000, 1000) {
-
+        // Se anima el fondo del contador.
+        animarFondoContador();
+        // Se inicia la cuenta atrás.
+        CountDownTimer contador = new CountDownTimer(CONTADOR_INICIAL,
+                CONTADOR_INTERVALO) {
+            // Cuando pasa un intervalo
             public void onTick(long millisUntilFinished) {
+                // Se actualiza el TextView con el valor del contador.
                 lblContador.setText((millisUntilFinished / 1000) + "");
             }
 
+            // Cuando la cuenta atrás llega a su fin.
             public void onFinish() {
-                animator.end();
+                // Se finaliza la animación del fono del contador.
+                mAnimador.end();
+                // Se hace visible el botón de comprobación y se oculta el
+                // contador.
                 btnComprobar.setVisibility(View.VISIBLE);
                 lblContador.setVisibility(View.GONE);
                 vContador.setVisibility(View.GONE);
             }
         }.start();
-
         super.onResume();
     }
 
+    // Inicia la animación de rotación del fondo del contador.
+    private void animarFondoContador() {
+        mAnimador = ObjectAnimator.ofFloat(vContador, View.ROTATION, 0.0f,
+                (CONTADOR_INICIAL / 1000) * 360.0f);
+        mAnimador.setDuration(CONTADOR_INICIAL);
+        mAnimador.setRepeatMode(ObjectAnimator.RESTART);
+        mAnimador.setRepeatCount(ObjectAnimator.INFINITE);
+        mAnimador.setInterpolator(new LinearInterpolator());
+        mAnimador.start();
+    }
+
+    // Obtiene e inicializa las vistas.
     private void getVistas() {
+        vContador = (View) findViewById(R.id.vContador);
+        lblPuntuacion = (TextView) findViewById(R.id.lblPuntuacion);
         btnComprobar = (Button) findViewById(R.id.btnComprobar);
+        // La actividad actuará como listener cuando se pulse el botón.
         btnComprobar.setOnClickListener(this);
         lblContador = (TextView) findViewById(R.id.lblContador);
         lstRespuestas = (ListView) this.findViewById(R.id.lstRespuestas);
+        // Se crea y asigna el adaptador a la lista.
         ArrayList<String> respuestas = new ArrayList<String>();
         respuestas.add("Marrón");
         respuestas.add("Verde");
         respuestas.add("Blanco");
         respuestas.add("Negro");
-        adaptador = new ArrayAdapter<String>(this, R.layout.respuesta,
+        mAdaptador = new ArrayAdapter<String>(this, R.layout.respuesta,
                 R.id.lblRespuesta, respuestas);
-        lstRespuestas.setAdapter(adaptador);
+        lstRespuestas.setAdapter(mAdaptador);
+        // Se trata de una lista de selección simple.
         lstRespuestas.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        // La actividad actuará como listener cuando se pulse sobre un
+        // elemento de la lista.
         lstRespuestas.setOnItemClickListener(this);
     }
 
+    // Cuando se pulsa el botón.
     @Override
     public void onClick(View arg0) {
+        // Se obtiene el elemento seleccionado.
         int posSeleccionado = lstRespuestas.getCheckedItemPosition();
         String elemSeleccionado = (String) lstRespuestas
                 .getItemAtPosition(posSeleccionado);
+        // Se comprueba si la respuesta es correcta.
         if (TextUtils.equals(elemSeleccionado, "Blanco")) {
-            Toast.makeText(this, "Bien", Toast.LENGTH_SHORT).show();
+            // Se muestra la puntuación.
+            lblPuntuacion.setText("+ " + mPuntuacion);
+            lblPuntuacion.setBackgroundResource(R.drawable.puntuacion_fondo);
+            animarPuntuacion();
         } else {
-            Toast.makeText(this, "Mal", Toast.LENGTH_SHORT).show();
+            // Se quita la puntuación.
+            int disminucion = mPuntuacion == 100 ? 50 : 25;
+            mPuntuacion -= disminucion;
+            lblPuntuacion.setText("- " + disminucion);
+            lblPuntuacion
+                    .setBackgroundResource(R.drawable.puntuacion_fondo_incorrecto);
+            animarPuntuacion();
+            // Se quita la selección.
             lstRespuestas.setItemChecked(posSeleccionado, false);
-            adaptador.remove(elemSeleccionado);
-            adaptador.notifyDataSetChanged();
+            // Se elimina dicha respuesta del adaptador y se fuerza
+            // el repintado de la lista.
+            mAdaptador.remove(elemSeleccionado);
+            mAdaptador.notifyDataSetChanged();
+            // Se desactiva el botón.
             btnComprobar.setEnabled(false);
         }
 
+    }
+
+    private void animarPuntuacion() {
+        lblPuntuacion.setVisibility(View.VISIBLE);
+        lblPuntuacion.animate().scaleX(1.5f).scaleY(1.5f).translationY(50)
+                .setDuration(3000).setInterpolator(new BounceInterpolator())
+                .setListener(new AnimatorListener() {
+
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        recolocarPuntuacion();
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                    }
+                });
+    }
+
+    private void recolocarPuntuacion() {
+        lblPuntuacion.setVisibility(View.INVISIBLE);
+        lblPuntuacion.setScaleX(1);
+        lblPuntuacion.setScaleY(1);
+        lblPuntuacion.setTranslationX(0);
+        lblPuntuacion.setTranslationY(0);
     }
 
     @Override
